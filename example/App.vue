502dccaa964b5d4fbb94a57b99655765965bdfb1
Socheat Sok <alex@socheat.net>
2021-11-01 10:50:39 +0700

    feat: update example

--- example/App.vue
+++ example/App.vue
@@ -7,35 +7,53 @@
 <script lang="ts">
 import Vue from 'vue'
 import VKbarProvider from '@/components/VKbarProvider.vue'
-import { KBarAction } from '@/interfaces'
+// import { KBarAction } from '@/interfaces'
 // import { mdiClipboard } from '@mdi/js'
-
-const actions: KBarAction[] = [
-  {
-    id: 'blog',
-    name: 'Blog',
-    // icon: mdiClipboard,
-    description: '',
-    shortcut: ['b'],
-    keywords: 'writing words',
-    perform: () => (window.location.pathname = 'blog')
-  },
-  {
-    id: 'contact',
-    name: 'Contact',
-    shortcut: ['c'],
-    keywords: 'email',
-    perform: () => (window.location.pathname = 'contact')
-  }
-]
 
 export default Vue.extend({
   name: 'App',
   components: {
     VKbarProvider
   },
-  data: () => ({
-    actions: actions
-  })
+  computed: {
+    actions () {
+      return [
+        {
+          id: 'blog',
+          name: 'Blog',
+          // icon: mdiClipboard,
+          description: '',
+          shortcut: ['b'],
+          keywords: 'writing words',
+          perform: () => (window.location.pathname = 'blog')
+        },
+        {
+          id: 'contact',
+          name: 'Contact',
+          shortcut: ['c'],
+          keywords: 'email',
+          perform: () => (window.location.pathname = 'contact')
+        },
+        {
+          id: 'theme-light',
+          name: 'Set light theme',
+          shortcut: ['t', 'h'],
+          keywords: 'light theme',
+          perform: () => {
+            this.$vuetify.theme.dark = false
+          }
+        },
+        {
+          id: 'theme-dark',
+          name: 'Set dark theme',
+          shortcut: ['t', 'h'],
+          keywords: 'dark theme',
+          perform: () => {
+            this.$vuetify.theme.dark = true
+          }
+        }
+      ]
+    }
+  }
 })
 </script>
