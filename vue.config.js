d82c665b03331d1f6573d4c66d9fa5714dc11b72
Socheat Sok <alex@socheat.net>
2021-11-01 11:50:18 +0700

    fix: docs generation

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 /** @type {import('@vue/cli-service').ProjectOptions} */
 module.exports = {
+  publicPath: process.env.BUILD_ENV === 'docs'
+    ? '/vuetify-kbar/'
+    : '/',
   css: {
     extract: false
   },
